31063141fa6a0e634c147f7f1ef5e216ee76c39e
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2019-01-06 01:06:51 +0000

    Attempting updates to fix the GC issue found with plugin
    Using more of the Kong pdk for 1.0 integration

--- src/handler.lua
+++ src/handler.lua
@@ -1,7 +1,6 @@
 local BasePlugin = require "kong.plugins.base_plugin"
 local tonumber = tonumber
 local MB = 2^20
-local str = "Response size limit exceeded"
 
 local KongResponseSizeLimitingHandler = BasePlugin:extend()
 KongResponseSizeLimitingHandler.PRIORITY = 802
@@ -14,8 +13,6 @@ local function check_size(length, allowed_size)
   local allowed_bytes_size = allowed_size * MB
   if length > allowed_bytes_size then
       kong.ctx.plugin.limited = true
-      kong.response.set_status(413)
-      kong.response.set_header("Content-Length", #str)
   end
 end
 
@@ -32,8 +29,7 @@ end
 function KongResponseSizeLimitingHandler:body_filter(conf)
   KongResponseSizeLimitingHandler.super.body_filter(self)
   if kong.ctx.plugin.limited then
-    ngx.arg[1] = str
-    ngx.arg[2] = true  
+     return kong.response.exit(413, "Response size limit exceeded")
   end
 end
 

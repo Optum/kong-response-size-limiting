396cb0c33608cd692d83ebb933fcbd9203ccdde1
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2019-01-07 05:10:35 +0000

    Trying different config
    Trying to prevent memory growth

--- src/handler.lua
+++ src/handler.lua
@@ -3,6 +3,7 @@ local tonumber = tonumber
 local MB = 2^20
 
 local responsestr = "{\"message\": \"Response size limit exceeded\"}"
+local sizetrigger = false
 
 local KongResponseSizeLimitingHandler = BasePlugin:extend()
 KongResponseSizeLimitingHandler.PRIORITY = 802
@@ -14,7 +15,7 @@ end
 local function check_size(length, allowed_size)
   local allowed_bytes_size = allowed_size * MB
   if length > allowed_bytes_size then
-      ngx.ctx.responsetoolarge = true
+      sizetrigger = true
       --kong.response.set_status(413)
       --kong.response.set_header("Content-Length", #responsestr)
       --kong.response.set_header("Content-Type", "application/json")
@@ -36,7 +37,7 @@ end
 
 function KongResponseSizeLimitingHandler:body_filter(conf)
   KongResponseSizeLimitingHandler.super.body_filter(self)
-  if ngx.ctx.responsetoolarge then		
+  if sizetrigger then		
     ngx.arg[1] = responsestr
     ngx.arg[2] = true
   end

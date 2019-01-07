cdde43037792e2d200a2f5755997d518d27aa5e6
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2019-01-07 03:01:16 +0000

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -2,6 +2,8 @@ local BasePlugin = require "kong.plugins.base_plugin"
 local tonumber = tonumber
 local MB = 2^20
 
+local responsestr = "{\"message:\" \"Response size limit exceeded\"}"
+
 local KongResponseSizeLimitingHandler = BasePlugin:extend()
 KongResponseSizeLimitingHandler.PRIORITY = 802
 
@@ -12,10 +14,7 @@ end
 local function check_size(length, allowed_size)
   local allowed_bytes_size = allowed_size * MB
   if length > allowed_bytes_size then
-      --kong.ctx.plugin.limited = true 
-      --ngx.header["Content-Type"] = "application/json"
-      --ngx.say("{ \"message\": \"Response size limit exceeded\"}")
-      ngx.exit(413)
+      ngx.ctx.responsetoolarge = true
   end
 end
 
@@ -29,11 +28,15 @@ function KongResponseSizeLimitingHandler:header_filter(conf)
   end
 end
 
---function KongResponseSizeLimitingHandler:body_filter(conf)
---  KongResponseSizeLimitingHandler.super.body_filter(self)
---  if kong.ctx.plugin.limited then
---     return kong.response.exit(413, "Response size limit exceeded")
---  end
---end
+function KongResponseSizeLimitingHandler:body_filter(conf)
+  KongResponseSizeLimitingHandler.super.body_filter(self)
+  if ngx.ctx.responsetoolarge then
+    kong.response.set_status(413)
+    kong.response.set_header("Content-Length", #responsestr)
+    kong.response.set_header("Content-Type", "application/json")		
+    ngx.arg[1] = responsestr
+    ngx.arg[2] = true
+  end
+end
 
 return KongResponseSizeLimitingHandler

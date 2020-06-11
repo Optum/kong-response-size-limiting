8b22fe2fce70144f1a03b955cdb6486c11de4da1
Ross Sbriscia <ross.sbriscia@optum.com>
2020-06-11 08:46:42 -0500

    update schema plug-in version 0.3

--- src/schema.lua
+++ src/schema.lua
@@ -1,5 +1,12 @@
+local typedefs = require "kong.db.schema.typedefs"
+
 return {
+  name = "kong-response-size-limiting",
   fields = {
-    allowed_payload_size = { default = 128, type = "number" }
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          { allowed_payload_size = { type = "number", default = 128 }, },
   }
 }
